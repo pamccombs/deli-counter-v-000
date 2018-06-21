c901699c20bb277c917259733df4d98fd81f7934
Patrick McCombs <pa.mccombs1@gmail.com>
2018-06-21 21:18:27 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,8 +15,9 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
+  katz_deli.unshift(name)
   if katz_deli.length == nil
-katz_deli.unshift(name)
+
 puts "Welcome #{katz_deli[0]}. You are number #{katz_deli.fecth(index)} in line."
   end
 end
