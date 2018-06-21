038be0568268c46113341ade75bc3d986790eab9
Patrick McCombs <pa.mccombs1@gmail.com>
2018-06-21 21:37:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,6 @@ puts "Welcome, #{katz_deli[0]}. You are number #{katz_deli.index(name) + 1} in l
 else
   katz_deli.length > 0 
   katz_deli << name
-
+puts "Welcome, #{katz_deli[0]}. You are number #{katz_deli.index(name) + 1} in line."
   end
 end
