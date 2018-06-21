a28b5132a359ab9180636a144ed9bac50d40c44c
Patrick McCombs <pa.mccombs1@gmail.com>
2018-06-21 21:13:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,22 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(katz_deli)
+ if katz_deli.length > 1
+   output = "The line is currently:"
+   katz_deli.each_with_index do |name, index|
+     output << " #{index + 1}. #{name}"
+   end
+   puts output
+ else
+   puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length == nil
+katz_deli.unshift(name)
+puts "Welcome #{katz_deli[0]}. You are number #{katz_deli.fecth(index)} in line."
+  end
+end
