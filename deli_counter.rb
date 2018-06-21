eed33ea47ec7bf6bb3a18111298a2af0ebc56f84
Patrick McCombs <pa.mccombs1@gmail.com>
2018-06-21 21:27:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
   
   if katz_deli.length == 0
 katz_deli << name
-puts "Welcome #{katz_deli[0]}. You are number #{katz_deli.index(name)} in line."
+puts "Welcome #{katz_deli[0]}. You are number #{katz_deli.index(name) + 1} in line."
 else
   end
 end
